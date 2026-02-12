38358377d4eea4ed3a8b3ec16080738668fdb7a5
MahmoudAkram21 <mahmoudakram422@gmail.com>
2026-02-12 15:07:53 +0200

    fix deploy issue

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,14 @@
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default {
-    apps : [{
-      name: "um-qura-backend",
-      script: "./dist/index.js",
-      instances: "max",
-      exec_mode: "cluster"
-    }]
-  };
+  apps: [{
+    name: "um-qura-backend",
+    script: join(__dirname, "dist", "index.js"),
+    cwd: __dirname,
+    instances: "max",
+    exec_mode: "cluster",
+  }],
+};
